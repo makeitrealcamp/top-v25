def1d0a15b3217142794435e4438e1a376c0176b
khriztianmoreno <khriztianmoreno@gmail.com>
2022-12-12 15:47:11 -0500

    feat: add exercise to email

--- examples/react-examples/src/App.jsx
+++ examples/react-examples/src/App.jsx
@@ -1,41 +1,19 @@
-import { useState, useEffect } from 'react'
+import LoginForm from './componets/LoginForm'
+import ListUsers from './componets/ListUsers'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [result, setResult] = useState([])
-
-  useEffect(() => {
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://swapi.dev/api/people')
-        const data = await response.json()
-        setResult(data.results)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    fetchData()
-
-  }, [])
 
 
   return (
     <div className="App">
-      <h1>{count}</h1>
-
-      <ul>
-        {
-          result.length
-            ? result.map((item) => <li key={item.id}>{item.name}</li>)
-            : <li>loading...</li>
-        }
-      </ul>
-
-      <button onClick={() => { setCount(count + 1) }}>+1</button>
+      <div className="login">
+        <LoginForm />
+      </div>
+      <div className="users">
+        <ListUsers />
+      </div>
     </div>
   )
 
