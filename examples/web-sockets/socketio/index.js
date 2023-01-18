0e7ef0018af88c4895bb28bf774cefd316eee274
khriztianmoreno <khriztianmoreno@gmail.com>
2023-01-18 15:26:08 -0500

    feat: add socketio example

--- examples/web-sockets/socketio/index.js
+++ examples/web-sockets/socketio/index.js
@@ -1,1 +1,33 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('message:add', (msg) => {
+    console.log('message', msg);
+    io.emit('message:add', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
+
+
